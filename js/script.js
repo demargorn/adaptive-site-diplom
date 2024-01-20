964a7d3c5b682b9f9a7c49e86c0122b8d6081550
Михаил <demargorn@gmail.com>
2024-01-20 17:34:39 +0300

    Правка кода js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 document.querySelector('.popup-close').onclick = function() {
-    // const closePopUp = document.querySelector('.popup');
-    // closePopUp.classList.add('visually-hidden');
     document.querySelector('.popup').classList.add('visually-hidden');
 };

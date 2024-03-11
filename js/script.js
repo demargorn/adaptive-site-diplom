04cf7bc9f1165443193439074931cdb984526298
Михаил <demargorn@gmail.com>
2024-03-11 21:33:01 +0300

    Правка кода

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-document.querySelector('.popup-close').onclick = function() {
+'use strict';
+
+// по нажатию на крестик pop-up закрывается
+document.querySelector('.popup__close-btn').onclick = function() {
     document.querySelector('.popup').classList.add('visually-hidden');
 };

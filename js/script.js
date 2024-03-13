f5a6f045ad4ceec86a83522ab97329647476d603
Михаил <demargorn@gmail.com>
2024-03-13 17:13:04 +0300

    Правка js-файла

--- js/script.js
+++ js/script.js
@@ -11,12 +11,3 @@ window.addEventListener('keydown', (e) => {
         document.querySelector('.popup').classList.add('visually-hidden');
     };
 });
-
-// закрытие pop-up при клике вне его
-document.querySelector('.popup').addEventListener('click', (e) => {
-    e._isClickWithInModal === true;
-});
-document.querySelector('.popup').addEventListener('click', (e) => {
-    if (e._isClickWithInModal) return;
-    e.currentTarget.classList.add('visually-hidden');
-});

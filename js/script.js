0f1c71b98bc359f03c55b9f98d8cde94ca297413
Михаил <demargorn@gmail.com>
2024-03-13 16:55:38 +0300

    Редактирование js-файла

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 'use strict';
 
-// по нажатию на крестик pop-up закрывается
+// при нажатии на крестик pop-up закрывается
 document.querySelector('.popup__close-btn').onclick = function() {
     document.querySelector('.popup').classList.add('visually-hidden');
 };
+
+// при нажатии на кнопку Escape pop-up закрывается
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        document.querySelector('.popup').classList.add('visually-hidden');
+    };
+});
+
+// закрытие pop-up при клике вне его
+document.querySelector('.popup').addEventListener('click', (e) => {
+    e._isClickWithInModal === true;
+});
+document.querySelector('.popup').addEventListener('click', (e) => {
+    if (e._isClickWithInModal) return;
+    e.currentTarget.classList.add('visually-hidden');
+});
